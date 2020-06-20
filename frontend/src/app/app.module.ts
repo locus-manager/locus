198c16fa337cee40befa25dd44fba980d9006728
Walace Rodrigues <walace@Mac-mini-de-Walace.local>
2020-06-20 20:13:25 -0300

    feat(register): layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,18 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { RegisterComponent } from './components/register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient} from "@angular/common/http";
+export function createTranslateLoad(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PoModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PoModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot( {
+          loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoad,
+            deps: [HttpClient]
+          }
+        })
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private translateService: TranslateService) {
+    translateService.addLangs(['en', 'pt']);
+    translateService.setDefaultLang['en'];
+    const browserLang = translateService.getBrowserLang();
+    translateService.use(browserLang.match(/en|pt/) ? browserLang : 'en');
+  }
+}
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+//TODO: Verificar pq não está traduzindo
+  public options = [
+    { label: this.translateService.instant('Entry'), value: 'checkin' },
+    { label: this.translateService.instant('Exit'), value: 'checkout' }
+  ];
+  constructor(private formBuilder: FormBuilder,
+              private translateService: TranslateService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group( {
+      id: [''],
+      name: [''],
+      email: [''],
+      phone: [''],
+      checkInDate: [''],
+      checkoutDate: [''],
+      placeId: ['']
+    });
   }
 
 }
