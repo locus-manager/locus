5c1a3d7be3b3df672ad29d1ef1b0bfde933015d6
Ricardo Mello <ricardo.mello@outlook.com>
2020-06-20 19:17:55 -0300

    feat(user): add user, places and user on places queries/mutations

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -17,31 +17,90 @@ const Place = objectType({
     t.model.id()
     t.model.name()
     t.model.code()
-  }
+  },
+})
+
+const UsersOnPlaces = objectType({
+  name: 'UsersOnPlaces',
+  definition(t) {
+    t.model.id()
+    t.model.placeId()
+    t.model.userId()
+    t.model.place()
+    t.model.user()
+    t.model.checkoutDate({type: 'Date'})
+    t.model.checkInDate({ type: 'Date'})
+  },
 })
 
 const Query = objectType({
   name: 'Query',
   definition(t) {
-    t.list.field('users', {
-      type: 'User',
-      resolve: (_parent, _args, ctx) => {
-        return ctx.prisma.user.findMany()
-      },
-    })
+    t.crud.users({ alias: 'users' })
+    t.crud.places({ alias: 'places' })
+    t.crud.usersOnPlaces({ alias: 'usersOnPlaces' })
   },
 })
 
 const Mutation = objectType({
   name: 'Mutation',
   definition(t) {
-    t.crud.createOneUser({ alias: 'signupUser' })
+    t.field('createUser', {
+      type: 'User',
+      args: {
+        name: stringArg({ nullable: false }),
+        email: stringArg({ nullable: false }),
+        phone: stringArg({ nullable: false }),
+      },
+      resolve: (_, { name, email, phone }, ctx) => {
+        return ctx.prisma.user.create({ data: { name, email, phone } })
+      },
+    })
+
+    t.field('createPlace', {
+      type: 'User',
+      args: {
+        name: stringArg({ nullable: false }),
+        code: stringArg({ nullable: false }),
+      },
+      resolve: (_, { name, code }, ctx) => {
+        return ctx.prisma.place.create({ data: { name, code } })
+      },
+    })
+
+    t.field('createUserOnPlace', {
+      type: 'User',
+      args: {
+        name: stringArg({ nullable: false }),
+        email: stringArg({ nullable: false }),
+        phone: stringArg({ nullable: false }),
+        type: stringArg({ nullable: false }),
+        code: stringArg({ nullable: false }),
+      },
+      resolve: async (_, { name, email, phone, type, code }, ctx) => {
+        const user = await ctx.prisma.user.upsert({
+          where: { email },
+          update: { name, phone },
+          create: { name, email, phone },
+        })
+        console.log(new Date().toISOString())
+        console.log(user);
+        return await ctx.prisma.usersOnPlaces.create({
+          data: {
+            user: { connect: { id: user.id } },
+            place: { connect: { code: code } },
+          },
+        })
+      },
+    })
   },
 })
 
 export const schema = makeSchema({
-  types: [Query, Mutation, User],
-  plugins: [nexusPrismaPlugin()],
+  types: [Query, Mutation, User, Place, UsersOnPlaces],
+  plugins: [nexusPrismaPlugin({
+    experimentalCRUD: true
+  })],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
