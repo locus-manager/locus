f9750b6b04d1283f2057f854249a4445c0ee090e
Ricardo Mello <ricardo.mello@outlook.com>
2020-06-20 16:47:23 -0300

    [WIP] backend

---
+++ backend/src/schema.ts
@@ -0,0 +1,53 @@
+import { intArg, makeSchema, objectType, stringArg } from '@nexus/schema'
+import { nexusPrismaPlugin } from 'nexus-prisma'
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.email()
+  },
+})
+
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.list.field('users', {
+      type: 'User',
+      resolve: (_parent, _args, ctx) => {
+        return ctx.prisma.user.findMany()
+      },
+    })
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOneUser({ alias: 'signupUser' })
+  },
+})
+
+export const schema = makeSchema({
+  types: [Query, Mutation, User],
+  plugins: [nexusPrismaPlugin()],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prisma',
+      },
+      {
+        source: require.resolve('./context'),
+        alias: 'Context',
+      },
+    ],
+  },
+})
