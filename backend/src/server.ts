756cde09ac7b3c06295dfb557f017624803031f2
Ricardo Mello <ricardo.mello@outlook.com>
2020-06-21 05:29:50 -0300

    ci: use dynamically assigned port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,9 @@ import { schema } from './schema'
 import { createContext } from './context'
 
 new ApolloServer({ schema, context: createContext }).listen(
-  { port: process.env.PORT || 5000 },
+  { port: process.env.PORT || 4000 },
   () =>
     console.log(
-      `🚀 Server ready at: http://localhost:8080\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
+      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
     ),
 )
